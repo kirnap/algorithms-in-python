10abb657bf3ff008785820ffd16d26a27c16dd5e
OmerKirnap <okirnap@ku.edu.tr>
2015-12-26 16:59:41 +0200

    Increase code readability

--- count_number_of_inversions.py
+++ count_number_of_inversions.py
@@ -15,54 +15,54 @@ I highly recommend first to understand your algorithm in the paper and then get 
 
 
 def sort_and_count(iterable):
-	"""
-	This function recursively calls itself to break up the list into two parts and then recursively recover them
-	the actual algorithm is implemented in this function
+    """
+    This function recursively calls itself to break up the list into two parts and then recursively recover them
+    the actual algorithm is implemented in this function
 
-	"""
-	if len(iterable) == 1:
-		return iterable, 0
-	else:
-		(sorted_left, left_inversion_number) = sort_and_count(iterable[:(len(iterable) / 2)])
-		(sorted_right, right_inversion_number) = sort_and_count(iterable[len(iterable) / 2:])
-		(result, split_inversion_number) = merge_and_count(sorted_left,sorted_right)
-		return result,left_inversion_number + right_inversion_number + split_inversion_number
+    """
+    if len(iterable) == 1:
+        return iterable, 0
+    else:
+        (sorted_left, left_inversion_number) = sort_and_count(iterable[:(len(iterable) / 2)])
+        (sorted_right, right_inversion_number) = sort_and_count(iterable[len(iterable) / 2:])
+        (result, split_inversion_number) = merge_and_count(sorted_left,sorted_right)
+        return result,left_inversion_number + right_inversion_number + split_inversion_number
 
 
 def find_inversions(iterable):
-	"""
-	Since sort_and_count function returns a tuple and I just need the number of inversions first part is enough for
-	me
-	List:parameter
-	Number:return
-	"""
-	return sort_and_count(iterable)[1]
+    """
+    Since sort_and_count function returns a tuple and I just need the number of inversions first part is enough for
+    me
+    List:parameter
+    Number:return
+    """
+    return sort_and_count(iterable)[1]
 
 
 def merge_and_count(first, second):
-	"""
-	It is explained during lecture that while implementing merge part there is a single path that
-	counts the inversions automatically if the second part of the
+    """
+    It is explained during lecture that while implementing merge part there is a single path that
+    counts the inversions automatically if the second part of the
 
-	"""
-	i = 0
-	j = 0
-	ret = []
-	number_of_inversion = 0
-	while len(ret) != len(first) + len(second):
-		if i == len(first):
-			ret += second[j:]
-		elif j == len(second):
-			ret += first[i:]
-		else:
-			if first[i] < second[j]:
-				ret.append(first[i])
-				i += 1
-			else:
-				ret.append(second[j])
-				j += 1
-				number_of_inversion += len(first) - i
-	return ret,number_of_inversion
+    """
+    i = 0
+    j = 0
+    ret = []
+    number_of_inversion = 0
+    while len(ret) != len(first) + len(second):
+        if i == len(first):
+            ret += second[j:]
+        elif j == len(second):
+            ret += first[i:]
+        else:
+            if first[i] < second[j]:
+                ret.append(first[i])
+                i += 1
+            else:
+                ret.append(second[j])
+                j += 1
+                number_of_inversion += len(first) - i
+    return ret,number_of_inversion
 
 
 
